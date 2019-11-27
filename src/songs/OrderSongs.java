c9cb6ec31096d9240c02039ef3acf2757c7d9c16
Liudmila <mehedinteanuliudmila@yahoo.com>
2019-11-27 18:51:12 +0100

    import project

--- src/songs/OrderSongs.java
+++ src/songs/OrderSongs.java
@@ -4,15 +4,25 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 
 public class OrderSongs {
     public static void main(String[] args) {
         try {
-            BufferedReader file = new BufferedReader((new FileReader("songs.txt")));
+            BufferedReader file = new BufferedReader(new FileReader("songs.txt"));
             String line;
-            while ((line = file.readLine()) != null) {
+            ArrayList<String> songs = new ArrayList<>();
 
+            while ((line = file.readLine()) != null) {
+                songs.add(line);
             }
+            System.out.println(songs.size());
+            System.out.println(songs);
+
+            Collections.shuffle(songs);
+            System.out.println(songs);
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         } catch (IOException e) {
