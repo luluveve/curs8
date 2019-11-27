20c47eba263095917fee1605832d450f1fdb23db
Liudmila <mehedinteanuliudmila@yahoo.com>
2019-11-27 17:22:03 +0100

    import project

---
+++ src/files/BufferedFileRead.java
@@ -0,0 +1,22 @@
+package files;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class BufferedFileRead {
+    public static void main(String[] args) {
+        try {
+            BufferedReader file = new BufferedReader(new FileReader("src/files/FileRead.java"));
+            String line;
+            while((line= file.readLine()) != null) {
+
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
