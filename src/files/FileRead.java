10fdc88d034904db192829af49ef7709c4df86fb
Liudmila <mehedinteanuliudmila@yahoo.com>
2019-11-27 17:12:00 +0100

    import project

--- src/files/FileRead.java
+++ src/files/FileRead.java
@@ -2,12 +2,20 @@ package files;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 
 public class FileRead {
     public static void main(String[] args) {
         try {
-            FileInputStream stream = new FileInputStream("FileRead.Java");
+            FileInputStream file = new FileInputStream("src/files/FileRead.java");
+            int c;
+            while((c = file.read()) != -1) {
+                    System.out.print((char) c);
+            }
+
         } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
