cb2c60e32c78f1ffc4840e2d008f4c8085626410
Liudmila <mehedinteanuliudmila@yahoo.com>
2019-11-27 19:10:51 +0100

    import project

---
+++ src/songs/OrderArtist.java
@@ -0,0 +1,32 @@
+package songs;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class OrderArtist {
+    public static void main(String[] args) {
+        try {
+            BufferedReader file = new BufferedReader(new FileReader("titles.txt"));
+            String line;
+            ArrayList<String> songs = new ArrayList<>();
+
+            while ((line = file.readLine()) != null) {
+                songs.add(line);
+            }
+            System.out.println(songs.size());
+            System.out.println(songs);
+
+            Collections.shuffle(songs);
+            System.out.println(songs);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
+}
