5c754b94d78f7fef17c2ce7d9025422dcab810b5
Liudmila <mehedinteanuliudmila@yahoo.com>
2019-11-27 17:46:33 +0100

    import project

--- src/songs/OrderSongs.java
+++ src/songs/OrderSongs.java
@@ -1,5 +1,22 @@
 package songs;
 
-public class OrderSongs {
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 
+public class OrderSongs {
+    public static void main(String[] args) {
+        try {
+            BufferedReader file = new BufferedReader((new FileReader("songs.txt")));
+            String line;
+            while ((line = file.readLine()) != null) {
+
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
