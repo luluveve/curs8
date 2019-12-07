ddbb7d0423ad4e6ef3b8ba23ebc889fd29fb8fec
Liudmila <mehedinteanuliudmila@yahoo.com>
2019-12-07 18:23:13 +0200

    import project

--- src/songs/Song.java
+++ src/songs/Song.java
@@ -1,5 +1,7 @@
 package songs;
 
+import java.util.Objects;
+
 public class Song {
     protected String title;
     protected String artist;
@@ -7,6 +9,20 @@ public class Song {
     public Song(String title, String artist) {
         this.title = title;
         this.artist = artist;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Song song = (Song) o;
+        return Objects.equals(title, song.title) &&
+                Objects.equals(artist, song.artist);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(title, artist);
     }
 
     @Override
--- src/songs/equalTest.java
+++ src/songs/equalTest.java
@@ -6,6 +6,16 @@ public class equalTest {
     public static void main(String[] args) {
         Song first = new Song("Come as you are", "Nirvana");
         Song second = new Song ("Come as you are", "Nirvana");
+
+
+
+        if (first.equals(second) ) {
+            System.out.println("Sunt egale");
+        }
+            else {
+                System.out.println("Nu sunt egale");
+            }
+        }
     }
 
-}
+
