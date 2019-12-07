1b1fd2eb6493b70492d14eda19556cce2b2b3061
Liudmila <mehedinteanuliudmila@yahoo.com>
2019-12-07 17:11:02 +0200

    import project

--- src/songs/OrderArtist.java
+++ src/songs/OrderArtist.java
@@ -6,13 +6,14 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashSet;
 
 public class OrderArtist {
     public static void main(String[] args) {
         try {
             BufferedReader file = new BufferedReader(new FileReader("titles.txt"));
             String line;
-            ArrayList<Song> songs = new ArrayList<>();
+            HashSet<Song> songs = new HashSet<>();
 
             while ((line = file.readLine()) != null) {
                 String [] parts = line.split("/");
@@ -22,8 +23,7 @@ public class OrderArtist {
             System.out.println(songs.size());
             System.out.println(songs);
 
-            Collections.sort(songs, new ArtistComparator());
-            System.out.println(songs);
+
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         } catch (IOException e) {
---
+++ src/songs/equalTest.java
@@ -0,0 +1,11 @@
+package songs;
+
+import songs.Song;
+
+public class equalTest {
+    public static void main(String[] args) {
+        Song first = new Song("Come as you are", "Nirvana");
+        Song second = new Song ("Come as you are", "Nirvana");
+    }
+
+}
